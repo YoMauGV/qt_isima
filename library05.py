0835b192aa0a8a0aab9a304cb2af6d14301b9dce
Mauro M.G.V. <isc.manuelgv@gmail.com>
2025-03-01 11:45:15 -0600

    Implementacion de Figuras y primera parte de color

--- library05.py
+++ library05.py
@@ -1,6 +1,7 @@
 from multimethod import multimethod
 from PyQt6 import QtGui
 from PyQt6.QtCore import Qt
+import math
 
 # Clase Geometrica, metaclase
 
@@ -27,6 +28,7 @@ class Circulo(Geometica):
         
     @multimethod    
     def dibujar(self):
+        self.painter.setBrush(Qt.GlobalColor.transparent)
         self.painter.drawEllipse(self.x-self.radio,self.y-self.radio,self.radio*2,self.radio*2)
 
     @multimethod    
@@ -50,4 +52,67 @@ class Cuadrado(Geometica):
 
     @multimethod    
     def dibujar(self):
+        self.painter.setBrush(Qt.GlobalColor.transparent)
         self.painter.drawRect(self.x,self.y,self.lado,self.lado)
+
+class Triangulo(Geometica):
+    def __init__(self, painter:QtGui.QPainter, x, y, lado):
+        super().__init__(painter, x, y)
+        self.lado = lado
+
+    @multimethod
+    def dibujar(self):
+        self.painter.setBrush(Qt.GlobalColor.transparent)
+        path = QtGui.QPainterPath()
+        path.moveTo(self.x, self.y)
+        path.lineTo(self.x + self.lado, self.y)
+        path.lineTo(self.x + self.lado/2, self.y - self.lado * 0.866)
+        path.closeSubpath()
+        self.painter.drawPath(path)
+
+    @multimethod
+    def dibujar(self, color:Qt.GlobalColor):
+        self.painter.setBrush(color)
+        path = QtGui.QPainterPath()
+        path.moveTo(self.x, self.y)
+        path.lineTo(self.x + self.lado, self.y)
+        path.lineTo(self.x + self.lado/2, self.y - self.lado * 0.866)
+        path.closeSubpath()
+        self.painter.drawPath(path)
+
+class Pentagono(Geometica):
+    def __init__(self, painter:QtGui.QPainter, x, y, radio):
+        super().__init__(painter, x, y)
+        self.radio = radio
+
+    @multimethod
+    def dibujar(self):
+        self.painter.setBrush(Qt.GlobalColor.transparent)
+        path = QtGui.QPainterPath()
+        angle = 72
+        for i in range(5):
+            theta = (angle * i -90) * math.pi / 180
+            x = self.x + self.radio * math.cos(theta)
+            y = self.y + self.radio * math.sin(theta)
+            if i == 0:
+                path.moveTo(x, y)
+            else:
+                path.lineTo(x, y)
+        path.closeSubpath()
+        self.painter.drawPath(path)
+
+    @multimethod
+    def dibujar(self, color:Qt.GlobalColor):
+        self.painter.setBrush(color)
+        path = QtGui.QPainterPath()
+        angle = 72
+        for i in range(5):
+            theta = (angle * i -90) * math.pi / 180
+            x = self.x + self.radio * math.cos(theta)
+            y = self.y + self.radio * math.sin(theta)
+            if i == 0:
+                path.moveTo(x, y)
+            else:
+                path.lineTo(x, y)
+        path.closeSubpath()
+        self.painter.drawPath(path)
